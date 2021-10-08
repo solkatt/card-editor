e4e29d5c1d192837c326d3aab621c6d640fb8d77
solkatt <viktoralexanderodman@gmail.com>
2021-10-08 02:59:42 +0200

    Update remove unused code

--- server/db/db.js
+++ server/db/db.js
@@ -23,18 +23,3 @@ mongoose
 const db = mongoose.connection
 
 module.exports = db
-
-// module.exports = {
-//   connectToServer: function (callback) {
-//     client.connect(function (err, db) {
-//       if (db) {
-//         _db = db.db('myFirstDatabase')
-//         console.log('Successfully connected to MongoDB')
-//       }
-//       return callback(err)
-//     })
-//   },
-//   getDb: function () {
-//     return _db
-//   }
-// }
